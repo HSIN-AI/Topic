37266e53a485326d16dd74a2ec78352b7b08dcc8
3B117161 <3b117161@gm.student.ncut.edu.tw>
2025-04-14 05:50:02 +0800

    data_1.dart

--- data_1.dart
+++ data_1.dart
@@ -3,18 +3,19 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:async'; // Import this package
-import 'home_page.dart';  // 引入 HomePage
-import 'profile_page.dart';  // 引入 ProfilePage
-import 'dashboard.dart'; // 引入 SensorDashboard
-import 'data_3.dart'; // 引入 Data3
-import 'data_5.dart'; // 引入 Data5
-import 'data_6.dart'; // 引入 Data6
-import 'cgatbot.dart'; // 引入 ChatBotPage
-import 'library_page.dart';  // 引入 LibraryPage
+import 'dart:async';
+
+import 'home_page.dart';
+import 'profile_page.dart';
+import 'dashboard.dart';
+import 'data_3.dart';
+import 'data_5.dart';
+import 'data_6.dart';
+import 'cgatbot.dart';
+import 'library_page.dart';
 
 class Data1 extends StatefulWidget {
-  const Data1({super.key});
+  const Data1({Key? key}) : super(key: key);
 
   @override
   _Data1State createState() => _Data1State();
@@ -22,13 +23,12 @@ class Data1 extends StatefulWidget {
 
 class _Data1State extends State<Data1> {
   List<TableRow> _tableRows = [];
-  Timer? _timer; // Timer object
+  Timer? _timer;
 
   @override
   void initState() {
     super.initState();
     _fetchData();
-    // Set up the timer to fetch data every 5 seconds
     _timer = Timer.periodic(Duration(seconds: 5), (Timer timer) {
       _fetchData();
     });
@@ -36,50 +36,49 @@ class _Data1State extends State<Data1> {
 
   Future<void> _fetchData() async {
     try {
-      final response =
-      await http.get(Uri.parse('https://gyyonline.uk/soil_conditions/'));
+      final response = await http.get(Uri.parse('https://gyyonline.uk/soil_moisture/'));
 
       if (response.statusCode == 200) {
-        final data = json.decode(response.body) as List;
-        List<TableRow> rows = [];
+        final jsonResult = json.decode(response.body);
 
-        // 添加表头
-        rows.add(_buildTableRow('時間戳記', '環境溫度（°C）', '濕度（%）', '是否需要澆水',
-            isHeader: true));
+        if (jsonResult is Map<String, dynamic> && jsonResult.containsKey('data')) {
+          final data = jsonResult['data'] as List;
 
-        // 通过循环添加数据行
-        for (var item in data) {
-          rows.add(
-            _buildTableRow(
-              item['timestamp'],
-              item['temperature'].toString(),
-              item['humidity'].toString(),
-              item['is_watered'] == 1 ? '是' : '否',
-            ),
-          );
+          List<TableRow> rows = [];
+          rows.add(_buildTableRow(['感測器', '數據序號', '類型', '時間戳', '濕度(%)'], isHeader: true));
+
+          for (var item in data) {
+            final sensor = item['sno'] ?? '無資料';
+            final cntNo = item['cnt_no']?.toString() ?? '無資料';
+            final typeId = item['type_id']?.toString() ?? '無資料';
+            final timestamp = item['timestamp'] ?? '無資料';
+            final value = item['value']?.toString() ?? '無資料';
+
+            rows.add(_buildTableRow([sensor, cntNo, typeId, timestamp, value]));
+          }
+
+          setState(() {
+            _tableRows = rows;
+          });
+        } else {
+          setState(() {
+            _tableRows = [_buildTableRow(['錯誤', '無 data 欄位', '無資料', '無資料', '無資料'])];
+          });
         }
-
-        setState(() {
-          _tableRows = rows;
-        });
       } else {
-        // Handle error response
         setState(() {
-          _tableRows = [];
+          _tableRows = [_buildTableRow(['錯誤', '狀態碼 ${response.statusCode}', '無資料', '無資料', '無資料'])];
         });
       }
     } catch (e) {
-      // Handle exceptions
-      print('Error fetching data: $e');
       setState(() {
-        _tableRows = [];
+        _tableRows = [_buildTableRow(['錯誤', '例外錯誤', '無資料', '無資料', '無資料'])];
       });
     }
   }
 
   @override
   void dispose() {
-    // Cancel the timer when the widget is disposed
     _timer?.cancel();
     super.dispose();
   }
@@ -87,16 +86,16 @@ class _Data1State extends State<Data1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // 添加Drawer
+      backgroundColor: Color(0xFFF1F8E9),
       drawer: Drawer(
         child: Container(
-          color: Color(0xFF262626),
+          color: Color(0xFFF1F8E9),
           child: ListView(
             padding: EdgeInsets.zero,
             children: [
               DrawerHeader(
                 decoration: BoxDecoration(
-                  color: Color(0xFF555555),
+                  color: Color(0xFFB9F6CA),
                 ),
                 child: Column(
                   children: [
@@ -116,224 +115,140 @@ class _Data1State extends State<Data1> {
                   ],
                 ),
               ),
-              ListTile(
-                leading: Icon(Icons.home, color: Colors.white),
-                title: Text(
-                  '首頁',
-                  style: GoogleFonts.inter(fontSize: 18, color: Colors.white),
-                ),
-                onTap: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.dashboard, color: Colors.white),
-                title: Text(
-                  '儀表板',
-                  style: GoogleFonts.inter(fontSize: 18, color: Colors.white),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SensorDashboard()),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.library_books, color: Colors.white),
-                title: Text(
-                  '圖書館',
-                  style: GoogleFonts.inter(fontSize: 18, color: Colors.white),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LibraryPage()),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.account_circle, color: Colors.white),
-                title: Text(
-                  '個人資料',
-                  style: GoogleFonts.inter(fontSize: 18, color: Colors.white),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ProfilePage()),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.wb_sunny, color: Colors.white),
-                title: Text(
-                  '土壤溫濕度',
-                  style: GoogleFonts.inter(fontSize: 18, color: Colors.white),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Data1()),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.thermostat, color: Colors.white),
-                title: Text(
-                  '葉面溫度',
-                  style: GoogleFonts.inter(fontSize: 18, color: Colors.white),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Data3()),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.eco, color: Colors.white),
-                title: Text(
-                  '碳排放',
-                  style: GoogleFonts.inter(fontSize: 18, color: Colors.white),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Data5()),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.water_drop, color: Colors.white),
-                title: Text(
-                  '酸鹼度',
-                  style: GoogleFonts.inter(fontSize: 18, color: Colors.white),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Data6()),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.chat_bubble, color: Colors.white),
-                title: Text(
-                  '阿吉同學',
-                  style: GoogleFonts.inter(fontSize: 18, color: Colors.white),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ChatBotPage(userQuery: ''),
-                    ),
-                  );
-                },
-              ),
+              _buildDrawerItem(Icons.home, '首頁', () {
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
+              }),
+              _buildDrawerItem(Icons.dashboard, '儀表板', () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => SensorDashboard()));
+              }),
+              _buildDrawerItem(Icons.library_books, '圖書館', () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => LibraryPage()));
+              }),
+              _buildDrawerItem(Icons.account_circle, '個人資料', () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ProfilePage()));
+              }),
+              _buildDrawerItem(Icons.wb_sunny, '土壤濕度', () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Data1()));
+              }),
+              _buildDrawerItem(Icons.thermostat, '葉面溫度', () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Data3()));
+              }),
+              _buildDrawerItem(Icons.eco, '碳排放', () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Data5()));
+              }),
+              _buildDrawerItem(Icons.water_drop, '酸鹼度', () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Data6()));
+              }),
+              _buildDrawerItem(Icons.chat_bubble, '阿吉同學', () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ChatBotPage(userQuery: '')));
+              }),
             ],
           ),
         ),
       ),
       appBar: AppBar(
-        backgroundColor: Color(0xFF262626),
-        title: const Text('土壤溫濕度'),
+        backgroundColor: Color(0xFF81C784),
+        title: const Text('土壤濕度'),
         centerTitle: true,
         leading: Builder(
           builder: (context) => IconButton(
             icon: Icon(Icons.menu),
-            onPressed: () {
-              Scaffold.of(context).openDrawer(); // 打開側邊攔
-            },
+            onPressed: () => Scaffold.of(context).openDrawer(),
           ),
         ),
       ),
       body: SingleChildScrollView(
         child: Container(
-          decoration: BoxDecoration(
-            color: Color(0xFF262626),
+          decoration: const BoxDecoration(
+            color: Color(0xFFF1F8E9),
           ),
-          padding: EdgeInsets.fromLTRB(18.5, 80.6, 18.5, 21),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: Container(
-                  width: 26,
-                  height: 21,
-                  child: SvgPicture.asset(
-                    'assets/vectors/vector_x2.svg',
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(18.5, 80.6, 18.5, 21),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: IconButton(
+                    icon: SvgPicture.asset(
+                      'assets/vectors/vector_12_x2.svg',
+                      width: 26,
+                      height: 21,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                   ),
                 ),
-              ),
-              Center(
-                child: Container(
-                  margin: const EdgeInsets.fromLTRB(29.4, 0, 29.4, 42),
-                  child: Text(
-                    '土壤溫濕度',
-                    style: GoogleFonts.getFont(
-                      'ABeeZee',
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 40,
-                      height: 0.5,
-                      color: const Color(0xFFFFFFFF),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: Text(
+                      '土壤濕度',
+                      style: GoogleFonts.getFont(
+                        'ABeeZee',
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 40,
+                        height: 1.2,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.fromLTRB(15.3, 0, 15.3, 66),
-                child: Table(
-                  columnWidths: const {
-                    0: FlexColumnWidth(2),
-                    1: FlexColumnWidth(1.2),
-                    2: FlexColumnWidth(1.2),
-                    3: FlexColumnWidth(0.8),
-                  },
-                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                  children: _tableRows,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.3),
+                  child: Table(
+                    columnWidths: const {
+                      0: FlexColumnWidth(2),
+                      1: FlexColumnWidth(1.4),
+                      2: FlexColumnWidth(1.4),
+                      3: FlexColumnWidth(1.4),
+                      4: FlexColumnWidth(1.0),
+                    },
+                    defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                    children: _tableRows,
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: 269.9,
-                height: 42,
-                child: SvgPicture.asset(
-                  'assets/vectors/component_41_x2.svg',
+                SizedBox(
+                  width: double.infinity,
+                  height: 42,
+                  child: SvgPicture.asset(
+                    'assets/vectors/component_41_x2.svg',
+                    fit: BoxFit.contain,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  TableRow _buildTableRow(
-      String time, String temperature, String humidity, String waterNeeded,
-      {bool isHeader = false}) {
-    return TableRow(
-      decoration: BoxDecoration(
-        color: isHeader ? const Color(0xFF444444) : Colors.transparent,
+  ListTile _buildDrawerItem(IconData icon, String title, VoidCallback onTap) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.black),
+      title: Text(
+        title,
+        style: GoogleFonts.inter(fontSize: 18, color: Colors.black),
       ),
-      children: [
-        _buildTableCell(time, isHeader: isHeader),
-        _buildTableCell(temperature, isHeader: isHeader),
-        _buildTableCell(humidity, isHeader: isHeader),
-        _buildTableCell(waterNeeded,
-            isHeader: isHeader, isWarning: waterNeeded == '是'),
-      ],
+      onTap: onTap,
     );
   }
 
-  Widget _buildTableCell(String text,
-      {bool isHeader = false, bool isWarning = false}) {
+  TableRow _buildTableRow(List<String> cells, {bool isHeader = false}) {
+    return TableRow(
+      decoration: BoxDecoration(
+        color: isHeader ? Color(0xFF81C784) : Colors.transparent,
+      ),
+      children: cells.map((cell) {
+        return _buildTableCell(cell, isHeader: isHeader);
+      }).toList(),
+    );
+  }
+
+  Widget _buildTableCell(String text, {bool isHeader = false}) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 6.0),
       child: Text(
@@ -343,9 +258,9 @@ class _Data1State extends State<Data1> {
           'ABeeZee',
           fontStyle: FontStyle.italic,
           fontWeight: isHeader ? FontWeight.bold : FontWeight.w400,
-          fontSize: 12,
+          fontSize: 14,
           height: 1.7,
-          color: isWarning ? const Color(0xFFFF0000) : const Color(0xFFFFFFFF),
+          color: Colors.black,
         ),
       ),
     );
